f17c96be955845af64a6a6d47734a2084248cb4d
Kenneth Amadi <amadikenneth12@yahoo.com>
2023-06-25 22:48:05 +0100

    Update project with addition of widgets alert_button.dart and suggestion_card.dart.

---
+++ lib/widgets/alert_button.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AlertButton extends StatefulWidget {
+  const AlertButton({super.key});
+
+  @override
+  State<AlertButton> createState() => _AlertButtonState();
+}
+
+class _AlertButtonState extends State<AlertButton> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/widgets/suggestion_card.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SuggestionCard extends StatelessWidget {
+  const SuggestionCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card();
+  }
+}
