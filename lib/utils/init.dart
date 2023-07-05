33c14730402755bc9d8b33d65df5061753278a73
Kenneth Amadi <amadikenneth12@yahoo.com>
2023-07-05 23:45:00 +0100

    Update project util files.

--- lib/utils/init.dart
+++ lib/utils/init.dart
@@ -9,15 +9,14 @@ LocationSettings get locationSettings {
     locationSettings = AndroidSettings(
       accuracy: LocationAccuracy.high,
       distanceFilter: 100,
-      forceLocationManager: true,
       intervalDuration: const Duration(seconds: 1),
       //(Optional) Set foreground notification config to keep the app alive
       //when going to the background
-      foregroundNotificationConfig: const ForegroundNotificationConfig(
+      /*foregroundNotificationConfig: const ForegroundNotificationConfig(
         notificationText: "Example app will continue to receive your location even when you aren't using it",
         notificationTitle: "Running in Background",
         enableWakeLock: true,
-      ),
+      ),*/
     );
   } else if (defaultTargetPlatform == TargetPlatform.iOS || defaultTargetPlatform == TargetPlatform.macOS) {
     locationSettings = AppleSettings(
