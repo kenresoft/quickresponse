76ab6ae92abc16a69d9bcfcc014d10785dc8325c
Kenneth Amadi <amadikenneth12@yahoo.com>
2023-07-27 22:16:00 +0100

    Update call.dart

--- lib/routes/call.dart
+++ lib/routes/call.dart
@@ -1,5 +1,9 @@
+import 'dart:developer';
+
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:extensionresoft/extensionresoft.dart';
 import 'package:flutter/material.dart';
+import 'package:quickresponse/data/constants/constants.dart';
 
 import '../data/constants/colors.dart';
 import '../data/constants/density.dart';
@@ -12,6 +16,8 @@ class Call extends StatefulWidget {
 }
 
 class _CallState extends State<Call> {
+  CarouselController buttonCarouselController = CarouselController();
+
   @override
   Widget build(BuildContext context) {
     final dp = Density.init(context);
@@ -28,11 +34,53 @@ class _CallState extends State<Call> {
               Text('Calling...', style: TextStyle(fontSize: 18, color: AppColor.white)),
               0.15.dpH(dp).spY,
               Text('Who needs help?', style: TextStyle(fontSize: 25, color: AppColor.white, fontWeight: FontWeight.w600)),
-              Expanded(child: ListView.builder(
-                itemBuilder: (context, i) {
-                  return Card();
-                },
-              ))
+              Expanded(
+                child: SizedBox(
+                  width: 0.9.dpW(dp),
+                  child: CarouselSlider.builder(
+                    itemCount: 3,
+                    itemBuilder: (BuildContext context, int itemIndex, int pageViewIndex) {
+                      log("$itemIndex : $pageViewIndex");
+                      return Center(
+                        child: Stack(
+                          children: [
+                            Positioned(
+                              left: 0,
+                              top: 0,
+                              right: 0,
+                              bottom: 0,
+                              //width: 100,
+                              //height: 100,
+                              child: Container(
+                                decoration: BoxDecoration(
+                                  color: Colors.red,
+                                  borderRadius: BorderRadius.circular(50),
+                                ),
+                              ),
+                            ),
+                            const Padding(
+                              padding: EdgeInsets.all(5.0),
+                              child: CircleAvatar(
+                                radius: 50,
+                                backgroundImage: AssetImage(Constants.profile),
+                              ),
+                            ),
+                          ],
+                        ),
+                      );
+                    },
+                    carouselController: buttonCarouselController,
+                    options: CarouselOptions(
+                      enableInfiniteScroll: false,
+                      enlargeCenterPage: true,
+                      viewportFraction: 0.3,
+                      height: 100,
+                      initialPage: 0,
+                      enlargeFactor: 0.6,
+                    ),
+                  ),
+                ),
+              )
             ]),
           )),
     );
