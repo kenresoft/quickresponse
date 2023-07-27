738e9d74eeb43b6cb499d8a7843186af5eaaa219
Kenneth Amadi <amadikenneth12@yahoo.com>
2023-07-27 22:49:18 +0100

    Update call.dart

--- lib/routes/call.dart
+++ lib/routes/call.dart
@@ -28,59 +28,60 @@ class _CallState extends State<Call> {
           color: AppColor.alertBorder,
           child: Center(
             child: Column(children: [
-              0.15.dpH(dp).spY,
+              0.2.dpH(dp).spY,
               Icon(Icons.ac_unit, color: AppColor.white, size: 40),
               Text('112', style: TextStyle(fontSize: 70, color: AppColor.white, fontWeight: FontWeight.w500)),
               Text('Calling...', style: TextStyle(fontSize: 18, color: AppColor.white)),
-              0.15.dpH(dp).spY,
+              0.25.dpH(dp).spY,
               Text('Who needs help?', style: TextStyle(fontSize: 25, color: AppColor.white, fontWeight: FontWeight.w600)),
-              Expanded(
-                child: SizedBox(
-                  width: 0.9.dpW(dp),
-                  child: CarouselSlider.builder(
-                    itemCount: 3,
-                    itemBuilder: (BuildContext context, int itemIndex, int pageViewIndex) {
-                      log("$itemIndex : $pageViewIndex");
-                      return Center(
-                        child: Stack(
+              SizedBox(
+                height: 120,
+                width: 310,
+                child: CarouselSlider.builder(
+                  itemCount: 3,
+                  itemBuilder: (BuildContext context, int itemIndex, int pageViewIndex) {
+                    log("$itemIndex : $pageViewIndex");
+                    return Center(
+                      child: Column(children: [
+                        Stack(
                           children: [
-                            Positioned(
+                            AnimatedPositioned(
                               left: 0,
                               top: 0,
                               right: 0,
                               bottom: 0,
-                              //width: 100,
-                              //height: 100,
+                              duration: const Duration(),
                               child: Container(
                                 decoration: BoxDecoration(
                                   color: Colors.red,
-                                  borderRadius: BorderRadius.circular(50),
+                                  borderRadius: BorderRadius.circular(100),
                                 ),
                               ),
                             ),
                             const Padding(
                               padding: EdgeInsets.all(5.0),
                               child: CircleAvatar(
-                                radius: 50,
+                                radius: 40,
                                 backgroundImage: AssetImage(Constants.profile),
                               ),
                             ),
                           ],
                         ),
-                      );
-                    },
-                    carouselController: buttonCarouselController,
-                    options: CarouselOptions(
-                      enableInfiniteScroll: false,
-                      enlargeCenterPage: true,
-                      viewportFraction: 0.3,
-                      height: 100,
-                      initialPage: 0,
-                      enlargeFactor: 0.6,
-                    ),
+                        Text('$itemIndex', style: TextStyle(fontSize: 18, color: AppColor.white)),
+                      ]),
+                    );
+                  },
+                  carouselController: buttonCarouselController,
+                  options: CarouselOptions(
+                    enableInfiniteScroll: false,
+                    enlargeCenterPage: true,
+                    viewportFraction: 0.35,
+                    aspectRatio: 1.0,
+                    enlargeFactor: 0.5,
+                    enlargeStrategy: CenterPageEnlargeStrategy.zoom,
                   ),
                 ),
-              )
+              ),
             ]),
           )),
     );
