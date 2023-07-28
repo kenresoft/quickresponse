7adbda412aff2baf0a8dbd23901202bb23cd6542
Kenneth Amadi <amadikenneth12@yahoo.com>
2023-07-28 06:25:54 +0100

    Update alert_button.dart

--- lib/widgets/alert_button.dart
+++ lib/widgets/alert_button.dart
@@ -10,7 +10,9 @@ class AlertButton extends StatefulWidget {
     required this.borderWidth,
     required this.shadowWidth,
     required this.iconSize,
-    this.showSecondShadow = true, this.iconData,
+    this.showSecondShadow = true,
+    this.iconData,
+    this.onTap,
   });
 
   final double height;
@@ -20,6 +22,7 @@ class AlertButton extends StatefulWidget {
   final double iconSize;
   final bool showSecondShadow;
   final IconData? iconData;
+  final GestureTapCallback? onTap;
 
   @override
   State<AlertButton> createState() => _AlertButtonState();
@@ -28,26 +31,29 @@ class AlertButton extends StatefulWidget {
 class _AlertButtonState extends State<AlertButton> {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-          borderRadius: BorderRadius.all(Radius.elliptical(widget.width, widget.height)),
-          gradient: AppColor.alert,
-          border: Border.all(width: widget.borderWidth, color: Colors.grey),
-          boxShadow: [
-            const BoxShadow(color: Colors.white, spreadRadius: 3),
-            widget.showSecondShadow ? BoxShadow(color: Colors.white.withOpacity(0.8), offset: const Offset(0, -20), blurRadius: 5) : const BoxShadow(),
-            BoxShadow(color: Colors.grey.withOpacity(0.2), offset: const Offset(10, 20), blurRadius: 5),
-            BoxShadow(color: Colors.grey.withOpacity(0.2), offset: const Offset(10, 0), blurRadius: 5),
-            BoxShadow(color: Colors.grey.withOpacity(0.2), offset: const Offset(-10, 10), blurRadius: 5),
-            BoxShadow(color: Colors.grey.withOpacity(0.2), offset: const Offset(-10, 0), blurRadius: 5),
-            BoxShadow(color: Colors.black.withOpacity(0.03), spreadRadius: widget.shadowWidth),
-          ]),
-      width: widget.width,
-      height: widget.height,
-      child: Icon(
-        widget.iconData??CupertinoIcons.waveform_path,
-        size: widget.iconSize,
-        color: CupertinoColors.white,
+    return GestureDetector(
+      onTap: () => widget.onTap,
+      child: Container(
+        decoration: BoxDecoration(
+            borderRadius: BorderRadius.all(Radius.elliptical(widget.width, widget.height)),
+            gradient: AppColor.alert,
+            border: Border.all(width: widget.borderWidth, color: Colors.grey),
+            boxShadow: [
+              const BoxShadow(color: Colors.white, spreadRadius: 3),
+              widget.showSecondShadow ? BoxShadow(color: Colors.white.withOpacity(0.8), offset: const Offset(0, -20), blurRadius: 5) : const BoxShadow(),
+              BoxShadow(color: Colors.grey.withOpacity(0.2), offset: const Offset(10, 20), blurRadius: 5),
+              BoxShadow(color: Colors.grey.withOpacity(0.2), offset: const Offset(10, 0), blurRadius: 5),
+              BoxShadow(color: Colors.grey.withOpacity(0.2), offset: const Offset(-10, 10), blurRadius: 5),
+              BoxShadow(color: Colors.grey.withOpacity(0.2), offset: const Offset(-10, 0), blurRadius: 5),
+              BoxShadow(color: Colors.black.withOpacity(0.03), spreadRadius: widget.shadowWidth),
+            ]),
+        width: widget.width,
+        height: widget.height,
+        child: Icon(
+          widget.iconData ?? CupertinoIcons.waveform_path,
+          size: widget.iconSize,
+          color: CupertinoColors.white,
+        ),
       ),
     );
   }
