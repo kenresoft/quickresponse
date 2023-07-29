c2fdad1824a7df1faa697bfb5e57f811f9ed8a16
Kenneth Amadi <amadikenneth12@yahoo.com>
2023-07-29 10:13:14 +0100

    Update contact.dart

--- lib/data/model/contact.dart
+++ lib/data/model/contact.dart
@@ -1,16 +1,21 @@
 class Contact {
-  Contact({required this.name, required this.relationship});
+  Contact({
+    required this.name,
+    required this.relationship,
+    this.phone,
+  });
 
-  final String name;
-  final String relationship;
+  final String? name;
+  final String? relationship;
+  final String? phone;
 
   static List<Contact> contactList = [
-    Contact(name: 'Mark Simmons', relationship: 'Husband'),
-    Contact(name: 'Jane Simmons', relationship: 'Daughter'),
-    Contact(name: 'James Simmons', relationship: 'Son'),
-    Contact(name: 'Margaret Mitchell', relationship: 'Mother'),
-    Contact(name: 'Leo Elon', relationship: 'Father'),
-    Contact(name: 'Howard Mitchell', relationship: 'Brother'),
-    Contact(name: 'Emma Mitchell', relationship: 'Sister'),
+    Contact(name: 'Mark Simmons', relationship: 'Husband', phone: '+123456789'),
+    Contact(name: 'Jane Simmons', relationship: 'Daughter', phone: '+123456789'),
+    Contact(name: 'James Simmons', relationship: 'Son', phone: '+123456789'),
+    Contact(name: 'Margaret Mitchell', relationship: 'Mother', phone: '+123456789'),
+    Contact(name: 'Leo Elon', relationship: 'Father', phone: '+123456789'),
+    Contact(name: 'Howard Mitchell', relationship: 'Brother', phone: '+123456789'),
+    Contact(name: 'Emma Mitchell', relationship: 'Sister', phone: '+123456789'),
   ];
 }
