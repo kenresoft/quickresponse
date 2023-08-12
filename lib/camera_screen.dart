19ef37fdcbf9613a94d93d2275146c65ce4c069e
Kenneth Amadi <amadikenneth12@yahoo.com>
2023-08-12 17:08:23 +0100

    Update camera_screen.dart

---
+++ lib/camera_screen.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:camera/camera.dart';
+
+import 'camera_preview.dart';
+
+
+class CameraScreen extends StatelessWidget {
+  const CameraScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: FutureBuilder<List<CameraDescription>>(
+        future: availableCameras(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            if (snapshot.hasError) {
+              // Handle error
+              return Center(child: Text('Error: ${snapshot.error}'));
+            }
+            if (snapshot.data == null || snapshot.data!.isEmpty) {
+              // No available cameras
+              return const Center(child: Text('No camera found on the device.'));
+            }
+
+            // Initialize the CameraController with the first available camera
+            return CameraPreviewWidget(camera: snapshot.data!.first);
+          } else {
+            // Loading indicator while waiting for the camera to be available
+            return const Center(child: CircularProgressIndicator());
+          }
+        },
+      ),
+    );
+  }
+}
