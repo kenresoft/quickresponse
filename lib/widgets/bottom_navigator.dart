83c6764a8e8d203e567168a47506a92ac79f1186
Kenneth Amadi <amadikenneth12@yahoo.com>
2023-08-22 21:59:24 +0100

    Update bottom_navigator.dart

--- lib/widgets/bottom_navigator.dart
+++ lib/widgets/bottom_navigator.dart
@@ -1,6 +1,7 @@
 import 'package:bottom_nav/bottom_nav.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:quickresponse/providers/page_provider.dart';
 
 import '../data/constants/colors.dart';
 import '../data/constants/constants.dart';
@@ -16,6 +17,7 @@ class BottomNavigator extends StatelessWidget {
   Widget build(BuildContext context) {
     return Consumer(
       builder: (BuildContext context, WidgetRef ref, Widget? child) {
+        var page = ref.watch(pageProvider.select((value) => value));
         return BottomNav(
           height: 68,
           // Attention: limit maximum height
@@ -24,8 +26,9 @@ class BottomNavigator extends StatelessWidget {
             ref.watch(tabProvider.notifier).setTab = index;
             if (index == 0) {
               replace(context, Constants.home);
+              ref.watch(pageProvider.notifier).setPage = page..add(Constants.home);
             } else if (index == 1) {
-              replace(context, Constants.contactDetails); 
+              replace(context, Constants.contactDetails);
             } else if (index == 2) {
               replace(context, Constants.contacts);
             } else if (index == 3) {
@@ -37,7 +40,7 @@ class BottomNavigator extends StatelessWidget {
           backgroundColor: AppColor.white,
           color: AppColor.navIcon,
           colorSelected: AppColor.navIconSelected,
-          indexSelected: currentIndex/*ref.watch(tabProvider.select((value) => value))*/,
+          indexSelected: currentIndex /*ref.watch(tabProvider.select((value) => value))*/,
           items: const [
             BottomNavItem(label: '', child: Icons.cabin),
             BottomNavItem(label: '', child: Icons.health_and_safety_outlined),
