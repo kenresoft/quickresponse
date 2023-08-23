91a0aede720cf5172fd54f561a344a2bc0190c1c
Kenneth Amadi <amadikenneth12@yahoo.com>
2023-08-23 23:02:23 +0100

    Update contact_details.dart

--- lib/routes/contacts.dart
+++ lib/routes/contacts.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:quickresponse/data/constants/constants.dart';
 import 'package:quickresponse/data/model/contact.dart';
 import 'package:quickresponse/main.dart';
@@ -7,28 +8,43 @@ import 'package:quickresponse/widgets/contact_searchbar.dart';
 
 import '../data/constants/colors.dart';
 import '../data/constants/density.dart';
+import '../providers/page_provider.dart';
 import '../widgets/bottom_navigator.dart';
+import '../widgets/exit_dialog.dart';
 
-class Contacts extends StatelessWidget {
+class Contacts extends ConsumerWidget {
   const Contacts({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, ref) {
     final contacts = Contact.contactList;
     final dp = Density.init(context);
-    return Scaffold(
-      backgroundColor: AppColor.background,
-      appBar: appBar(title: const Text('Contacts'), actionTitle: 'Add', actionIcon: Icons.add),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 15),
-        child: ContactSearchBar(
-          contacts: contacts,
-          onSelected: (Contact value) {
-            launch(context, Constants.contactDetails, value);
-          },
+    final page = ref.watch(pageProvider.select((value) => value));
+    return WillPopScope(
+      onWillPop: () async {
+        bool isLastPage = page.isEmpty;
+        if (isLastPage) {
+          return (await showAnimatedDialog(context))!;
+        } else {
+          launch(context, page.last);
+          ref.watch(pageProvider.notifier).setPage = page..remove(page.last);
+          return true;
+        }
+      },
+      child: Scaffold(
+        backgroundColor: AppColor.background,
+        appBar: appBar(title: const Text('Contacts'), actionTitle: 'Add', actionIcon: Icons.add),
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 15),
+          child: ContactSearchBar(
+            contacts: contacts,
+            onSelected: (Contact value) {
+              launch(context, Constants.contactDetails, value);
+            },
+          ),
         ),
+        bottomNavigationBar: const BottomNavigator(currentIndex: 2),
       ),
-      bottomNavigationBar: const BottomNavigator(currentIndex: 2),
     );
   }
 }
