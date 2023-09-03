8fbde3c092f677d1f7ba67ce5bb7d83cb1585247
Kenneth Amadi <amadikenneth12@yahoo.com>
2023-09-03 23:41:03 +0100

    Update project.

--- lib/utils/extensions.dart
+++ lib/utils/extensions.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
+import 'density.dart';
+
 extension Dp on num {
   double dpH(Density density) {
     if (density.height > density.width) {
