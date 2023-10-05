97c08278159c24f38d7ab551e511363884b5a1f9
Kenneth Amadi <amadikenneth12@yahoo.com>
2023-10-05 20:01:50 +0100

    Update project.

---
+++ lib/providers/settings/date_separator.dart
@@ -0,0 +1,13 @@
+/*
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final dateSeparatorProvider = StateNotifierProvider<DateSeparatorNotifier, String>((ref) {
+  return DateSeparatorNotifier();
+});
+
+class DateSeparatorNotifier extends StateNotifier<String> {
+  DateSeparatorNotifier() : super('/'); // Set the default separator
+
+  set setDateSeparator(String dateSeparator) => state = dateSeparator;
+}
+*/
