457ddd0d49c7f87b4b9dc783eb7ccf13cbddcc95
Kenneth Amadi <amadikenneth12@yahoo.com>
2023-10-06 22:52:09 +0100

    Update project.

--- lib/data/constants/styles.dart
+++ lib/data/constants/styles.dart
@@ -1,12 +1,11 @@
 import 'package:flutter/material.dart';
 
-import '../../main.dart';
-import '../../providers/providers.dart';
+import '../../providers/settings/prefs.dart' as pref;
 
 part '../../utils/app_color.dart';
 
 part '../../utils/density.dart';
 
-var isDarkTheme = providerContainer.read(themeProvider);
+//var isDarkTheme = providerContainer.read(themeProvider);
 
 var appColor = AppColor._();
