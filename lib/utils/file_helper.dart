f44baa8d696c8add87fbd65718eaaab14aeff43f
kenresoft <amadikenneth8@gmail.com>
2023-12-24 23:31:28 +0100

    Update project.

--- lib/utils/file_helper.dart
+++ lib/utils/file_helper.dart
@@ -1,13 +1,6 @@
-import 'dart:io';
 
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
-import 'package:path_provider/path_provider.dart';
 import 'package:quickresponse/imports.dart';
-import 'package:quickresponse/utils/extensions.dart';
 
-import '../data/constants/styles.dart';
-import '../providers/settings/prefs.dart';
 
 class FileHelper {
   static String getTempFilePath(String fileName) {
