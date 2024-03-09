ba802d9ca0fef9f7a020918588a9c97c14c008c4
kenresoft <amadikenneth8@gmail.com>
2024-03-10 00:18:39 +0100

    Update README.md

--- lib/data/constants/api.dart
+++
@@ -1,7 +0,0 @@
-class Api {
-  Api._();
-
-  static String googleAPIKey = "AIzaSyDZF_oNL8olqMqAbgcjl6HU1vj2eEjx1Hw";
-  static const String apiKey = 'fsq3tBBmB4MPuxulLcwftLTm78kwfz0FOdPRN3AgUonAL5I=';
-  static const String openRouteAPIKey = '5b3ce3597851110001cf624845eab8e52e2b413aa1473372f592099d';
-}
